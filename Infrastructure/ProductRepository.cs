bcb8cce78257d7cb1f7e64fd1cf75c4e40e8ca12
Vikramwani06 <viki4224.wani@gmail.com>
2020-08-29 23:22:58 -0400

    End of Section 7

--- Infrastructure/ProductRepository.cs
+++ Infrastructure/ProductRepository.cs
@@ -15,15 +15,16 @@ namespace Infrastructure
             _context = context;
         }
 
-       public async Task<Product> GetProductByIdAsync(int id)
+        public async Task<Product> GetProductByIdAsync(int id)
         {
             return await _context.Products
             .Include(p => p.ProductType)
             .Include(p => p.ProductBrand)
-            .FirstOrDefaultAsync(p => p.Id ==id);
+            .FirstOrDefaultAsync(p => p.Id == id);
         }
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
+
             return await _context.Products
             .Include(p => p.ProductType)
             .Include(p => p.ProductBrand)
